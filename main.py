bd3a84a5ce27b6788bd8ef14ebd09be487b488bc
Varun Srinivasan <varunsrin@gmail.com>
2015-02-10 22:40:14 -0800

    Create MP3 output

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 
 import sys
+from pydub import AudioSegment
 
 path = ''
 if sys.argv[1] == "-i":
   path = sys.argv[2]
-  print "Source: " + path
 else:
   raise Exception("no source file provided in the format main.py -i <path to file>")
+
+destination = path + ".mp3"
+
+try:
+  AudioSegment.from_file(path).export(destination, format='mp3')
+except IOError:
+  print "Source file " + path + " not found"
+  print IOError
+
